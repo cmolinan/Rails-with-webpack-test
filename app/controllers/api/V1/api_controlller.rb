d02b9524000a0b40c70e8a831f051d08fe11f01c
cmolinan <cmolinan@gmail.com>
2023-01-03 19:32:37 -0500

    Fixes to API Endpoint

--- app/controllers/api/V1/api_controlller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class ApiController < ActionController::API
-      def not_found
-        render json: { error: 'not_found' }
-      end
-    end
-  end
-end
--- app/controllers/api/V1/greetings_controller.rb
+++ app/controllers/api/V1/greetings_controller.rb
@@ -1,8 +1,6 @@
-module Api
-  module V1
-    def index
-      message = Greeting.order(Arel.sql('RANDOM()')).first
-      render json: { status: 'SUCCESS', message: 'Random message generated', data: message }, status: :ok
-    end
+class Api::V1::GreetingsController < ApplicationController
+  def index
+    message = Greeting.order(Arel.sql('RANDOM()')).first
+    render json: { status: 'SUCCESS', message: 'Random message generated', data: message }, status: :ok
   end
 end
